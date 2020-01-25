6c2c5bba8de2373b15be871460b6757caf3b9ea5
lithiumice <2729020028@qq.com>
2020-01-25 20:13:46 +0800

    demo

---
+++ src/main.c
@@ -0,0 +1,71 @@
+#include "main.h"
+#include "gpio.h"
+// #include "usart.h"
+#include "delay.h"
+
+void SystemClock_Config(void);
+void Delay(__IO uint32_t nCount);
+
+int main(void)
+{
+  HAL_Init();
+  SystemClock_Config();
+  MX_GPIO_Init();
+
+  uint16_t t, len;	
+	uint32_t times=0;  
+  
+	HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_2);
+  delay_init(168);
+
+  while (1)
+  {
+    HAL_GPIO_WritePin(GPIOF,GPIO_PIN_9|GPIO_PIN_10,1);
+		delay_ms(1000);
+    HAL_GPIO_WritePin(GPIOF,GPIO_PIN_9|GPIO_PIN_10,0);
+		
+  }
+}
+
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage 
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    // Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    // Error_Handler();
+  }
+}
+
+void Delay(__IO uint32_t nCount)
+{
+  while(nCount--){}
+}
